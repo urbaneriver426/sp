4ecbeb1e0b8e015df431cde5ca1fabf1917efac6
urbaneriver426 <45755209+urbaneriver426@users.noreply.github.com>
2019-05-16 22:53:26 +0300

    Update LineAnalysis.py

--- LineAnalysis.py
+++ LineAnalysis.py
@@ -1,9 +1,23 @@
 def LineAnalysis(line):
-    for i in range(len(line)):
-        if i == 0 or i % 3 == 0:
-            if line[i] != "*":
-                return False
-        else:  
-            if line[i] != ".":
-                return False
+    count = 1
+    point = 0
+
+    if line[0] != "*" or line[len(line)-1] != "*":
+        return False
+
+    for i in range(1,len(line)):
+        if point == 0:
+            if line[i] == ".":
+                count += 1
+            elif line[i] == "*":
+                point = i
+        else:
+            if line[i] == "*":
+                if i - point == count:
+                    point = i
+                else:
+                    return False
+            elif line[i] == ".":
+                if i - point >= count:
+                    return False
     return True
