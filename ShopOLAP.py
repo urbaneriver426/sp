56cab64d4f8d4a2a928aaa6400c78457a3e67a81
urbaneriver426 <45755209+urbaneriver426@users.noreply.github.com>
2019-05-19 21:59:03 +0300

    Update ShopOLAP.py

--- ShopOLAP.py
+++ ShopOLAP.py
@@ -1,23 +1,27 @@
 def ShopOLAP (items):
     checkedItems = []
     prices = []
+    result = []
+    def LastLetter(s):
+        return s[-1]
 
     for i in range(len(items)):
+        count = 0
         if i == 0:
             checkedItems.append(items[0][0:len(items[0])-1])
-            prices.append(int(items[0][len(items[0])-1:len(items[0])]))
+            prices.append(int(items[0][-1]))
         else:
             item = items[i][0:len(items[i])-1]
-            price = int(items[i][len(items[i])-1:len(items[i])])
+            price = int(items[i][-1])
             for j in range(len(checkedItems)):
                 if item == checkedItems[j]:
                     prices[j] += price
-                else:
+                    break
+                elif j == len(checkedItems)-1:
                     checkedItems.append(items[i][0:len(items[i])-1])
-                    prices.append(int(items[i][len(items[i])-1:len(items[i])]))
+                    prices.append(int(items[i][-1]))
         if i == len(items)-1:
             for j in range(len(checkedItems)):
                 checkedItems[j] += str(prices[j])
-            checkedItems.sort()
-    
-    return checkedItems
+        checkedItems.sort()
+        checkedItems.sort(key=LastLetter, reverse=True)
