fca3fe5641a04eb77daea5b3b35ecbad5ff10d3c
urbaneriver426 <45755209+urbaneriver426@users.noreply.github.com>
2019-06-02 23:27:29 +0300

    Update ShopOLAP.py

--- ShopOLAP.py
+++ ShopOLAP.py
@@ -1,30 +1,43 @@
 def ShopOLAP (n, items):
-    checkedItems = []
-    prices = []
-    result = []
-    def LastLetter(s):
-        return s[-1]
+	checkedItems = []
+	prices = []
 
-    for i in range(n):
-        count = 0
-        if i == 0:
-            checkedItems.append(items[0][0:len(items[0])-1])
-            prices.append(int(items[0][-1]))
-        else:
-            item = items[i][0:len(items[i])-1]
-            price = int(items[i][-1])
-            for j in range(len(checkedItems)):
-                if item == checkedItems[j]:
-                    prices[j] += price
-                    break
-                elif j == len(checkedItems)-1:
-                    checkedItems.append(items[i][0:len(items[i])-1])
-                    prices.append(int(items[i][-1]))
-        if i == len(items)-1:
-            for j in range(len(checkedItems)):
-                checkedItems[j] += str(prices[j])
-        checkedItems.sort()
-        checkedItems.sort(key=LastLetter, reverse=True)
+	def LastNumber(s):
+		x = ""
+		count = 0
+		for i in range(len(s)):
+			if count != 0:
+				x += s[i]
+			elif s[i] == " ":
+				count = 1
+		return int(x)
 
-    
-    return checkedItems
+	for i in range(len(items)):
+		count = 0
+		for j in range(len(items[i])-1,0,-1):
+			if items[i][j] != " ":
+				count += 1
+			else:
+				item = items[i][0:len(items[i])-count]
+				price = int(items[i][len(items[i])-count:len(items[i])])
+				if i == 0:
+					checkedItems.append(item)
+					prices.append(price)
+				else:
+					for k in range(len(checkedItems)):
+						if item == checkedItems[k]:
+							prices[k] += price
+							break
+						elif k == len(checkedItems)-1:
+							checkedItems.append(item)
+							prices.append(price)
+				break
+		if i == len(items)-1:
+			for j in range(len(checkedItems)):
+				checkedItems[j] = checkedItems[j] + str(prices[j])
+			prices.sort(reverse=True)
+
+	checkedItems.sort()
+	checkedItems.sort(key=LastNumber, reverse=True)
+
+	return checkedItems
